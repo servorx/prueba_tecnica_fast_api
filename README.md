0b2ed94d3f32cdc70e9b3540187b01b1efdd9c5a
servorx <191423123+servorx@users.noreply.github.com>
2025-11-06 13:23:59 -0500

    feat: :construction: proto structure and data

---
+++ README.md
@@ -0,0 +1,23 @@
+# Proycto prueba tecnica lectura de excel 
+
+## Paso a paso para ejecutar el proyecto
+
+### Clonar el repositorio
+```bash
+git clone https://github.com/servorx/prueba_tecnica_fast_api
+cd prueba_tecnica
+code . 
+```
+
+### Instalar dependencias
+```bash
+pip install -r requirements.txt
+```
+
+
+## .env de prueba 
+se realiza la conexion con el servidor de MySQL por lo que no se necesita XAMPP
+```bash
+DB_URL="mysql://campus2023:campus2023@localhost:3306/prueba_tecnica?schema=public"
+```
+
---
+++ app/data/dll.sql
@@ -0,0 +1,33 @@
+CREATE TABLE customers ( 
+    customer_id BIGINT PRIMARY KEY, 
+    name VARCHAR(180) NOT NULL, 
+    email VARCHAR(180), 
+    phone VARCHAR(40), 
+    address_line VARCHAR(255), 
+    city VARCHAR(120), 
+    state VARCHAR(120), 
+    country VARCHAR(120), 
+    postal_code VARCHAR(20) 
+) ENGINE=InnoDB; 
+
+CREATE TABLE orders ( 
+    order_id BIGINT PRIMARY KEY, 
+    order_date DATE NOT NULL, 
+    customer_id BIGINT NOT NULL, 
+    status ENUM('paid','pending','canceled') NOT NULL, 
+    shipping_method ENUM('standard','express','pickup'), 
+    coupon_code VARCHAR(40), 
+    CONSTRAINT fk_orders_customer FOREIGN KEY (customer_id) REFERENCES customers(customer_id) 
+) ENGINE=InnoDB; 
+
+CREATE TABLE order_items ( 
+    id BIGINT PRIMARY KEY AUTO_INCREMENT, 
+    order_id BIGINT NOT NULL, sku VARCHAR(40) NOT NULL, 
+    qty INT NOT NULL, 
+    unit_price DECIMAL(12,2) NOT NULL, 
+    line_total DECIMAL(12,2) AS (qty * unit_price) STORED, 
+    CONSTRAINT fk_items_order FOREIGN KEY (order_id) REFERENCES orders(order_id) ON DELETE CASCADE 
+) ENGINE=InnoDB; 
+
+CREATE INDEX idx_orders_customer ON orders(customer_id); 
+CREATE INDEX idx_items_order ON order_items(order_id);
